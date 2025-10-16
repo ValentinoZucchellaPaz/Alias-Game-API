6bfc229a7f754c2d5b800fd457f2dcea9dcc1159
piragna <gonzalovarelaalagna@gmail.com>
2025-10-16 15:29:39 -0300

    feat(socket): add '/admin' namespace with connection handler

--- src/server.js
+++ src/server.js
@@ -16,32 +16,31 @@ import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const socketApp = express();
 const SVPORT = process.env.SVPORT || 4000;
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const socketApp = express();
 //Serve static files from /client
 socketApp.use(express.static(path.join(__dirname, "client")));
 
 const server = http.createServer(socketApp);
-const io = new Server(server);
 server.listen(SVPORT, () => {
   console.log("WebSocket server on http://localhost:" + SVPORT);
 });
-const userIo = io.of("/user");
-userIo.on("connection", (socket) => {
-  console.log("conected to /user namespace");
+const io = new Server(server);
+const adminIo = io.of("/admin");
+
+adminIo.on("connection", (socket) => {
+  console.log(`socket ${socket} has conected to /admin namespace`);
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
-
   socket.on("send-message", (message, room) => {
-    console.log(message);
     if (room === "") {
-      socket.broadcast.emit("receive-message", message);
+      socket.broadcast.emit("receive-message", message); //message to all users in the namespace
     } else {
-      socket.to(room).emit("receive-message", message);
+      socket.to(room).emit("receive-message", message); //message to users in the specified room
     }
   });
   socket.on("join-room", (room, cb) => {
