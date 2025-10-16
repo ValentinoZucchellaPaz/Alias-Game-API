9b223de48867f6d4984afe50d806ab13af8b7fbf
piragna <gonzalovarelaalagna@gmail.com>
2025-10-16 10:39:00 -0300

    feat(server): add separate WebSocket server on SVPORT

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+
+//FOR API REST
 import app from "./app.js";
 import { syncDB } from "./models/sequelize/index.js";
-
 const PORT = process.env.PORT || 3000;
 
 (async () => {
@@ -8,3 +9,23 @@ const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
 })();
+
+
+//FOR WEB SOCKETS
+import http from "http";
+import {Server} from "socket.io";
+
+const SVPORT = process.env.SVPORT || 4000;
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
+
+server.listen(SVPORT, () => {
+  console.log("WebSocket server on http://localhost:"+SVPORT);
+});
+
+
