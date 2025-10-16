a18f8e3e91805d2b6314404de91ba13d315085c0
piragna <gonzalovarelaalagna@gmail.com>
2025-10-16 15:53:02 -0300

    feat(client): handle '/admin' namespace connection error with token-based auth

--- src/client/script.js
+++ src/client/script.js
@@ -4,7 +4,9 @@ const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
 
 const socket = io("http://localhost:4000");
-const adminSocket = io("http://localhost:4000/admin", { auth: { token:'test'   }  }  );
+
+// const adminSocket = io("http://localhost:4000/admin"); //will throw error
+const adminSocket = io("http://localhost:4000/admin", { auth: { token:'test'   }  }  ); //will not throw
 
 socket.on("connect", () => {
   displayMessage(`You've connected with id: ${socket.id}`);
@@ -13,7 +15,12 @@ socket.on("connect", () => {
 socket.on("receive-message", (message) => {
   displayMessage(message);
 });
-
+//new
+adminSocket.on('connect_error', (error) => {
+  displayMessage(error);
+})
+//new
+//actions taken when submitting form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = messageInput.value;
@@ -27,6 +34,7 @@ form.addEventListener("submit", (e) => {
   messageInput.value = "";
 });
 
+//actions taken when clicking on 'join'
 joinRoomButton.addEventListener("click", (e) => {
   const room = roomInput.value;
   socket.emit("join-room", room, (message) => {
@@ -34,6 +42,7 @@ joinRoomButton.addEventListener("click", (e) => {
   });
 });
 
+//info display method
 function displayMessage(message) {
   const div = document.createElement("div");
   div.textContent = message;
