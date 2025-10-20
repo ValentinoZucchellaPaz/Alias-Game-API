62117c1be89c3ddf9a074a48325be3d554a63e07
piragna <gonzalovarelaalagna@gmail.com>
2025-10-20 18:45:47 -0300

    chore(client): confirm no changes in room logic and UI reset

--- src/client/script.js
+++ src/client/script.js
@@ -4,6 +4,7 @@ const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
 const joinRedButton = document.getElementById("join-red-button");
 const joinBlueButton = document.getElementById("join-blue-button");
+const leaveRoomButton = document.getElementById('leave-room');
 
 //CONNECTION TO MULTIPLE NAMESPACES
 const socket = io("http://localhost:4000");
@@ -52,9 +53,24 @@ form.addEventListener("submit", (e) => {
 //join room
 joinRoomButton.addEventListener("click", (e) => {
   const room = roomInput.value;
+  console.log('trying to join', room)
   socket.emit("join-room", room, (message) => {
     displayMessage(message);
     document.getElementById('room-name').textContent= room;
+  });
+});
+
+//leave room
+leaveRoomButton.addEventListener('click', () => {
+  const room = roomInput.value;
+  if (!room) return;
+  socket.emit('leave-room', room, ()=>{
+    //clean interface
+    document.getElementById('room-name').textContent = 'None';
+    document.getElementById('message-container').innerHTML='';
+    document.getElementById('red-team-list').innerHTML='';
+    document.getElementById('blue-team-list').innerHTML='';
+    displayMessage(`You have left room ${room}`);
   });
 });
 
@@ -63,22 +79,19 @@ socket.on('host-assigned', ({room})=>{
   displayMessage(`You are now the host of room ${room}`);
 })
 
+
+//update teams' information
+socket.on("team-state", ({ red, blue }) => {
+  updateTeamList("red", red);
+  updateTeamList("blue", blue);
+});
+
 //info display method
 function displayMessage(message) {
   const div = document.createElement("div");
   div.textContent = message;
   document.getElementById("message-container").append(div);
 }
-
-socket.on("team-state", ({ red, blue }) => {
-  updateTeamList("red", red);
-  updateTeamList("blue", blue);
-});
-
-
-
-
-
 
 function updateTeamList(team, players) {
   const container = document.getElementById(`${team}-team-list`);
