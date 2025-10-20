4eef43a86b1c40433357e094d514c295a24448e7
piragna <gonzalovarelaalagna@gmail.com>
2025-10-20 19:45:54 -0300

    fix(socket): ensure team exclusivity and auto-delete empty rooms

--- src/sockets/default.js
+++ src/sockets/default.js
@@ -20,48 +20,63 @@ export default function setupDefaultNamespace(io) {
     socket.on("join-room", (room, cb) => {
       socket.join(room);
       //If there is no host, the socket becomes the host
-      if (!roomHosts[room]){
+      if (!roomHosts[room]) {
         roomHosts[room] = socket.id;
-        socket.emit('host-assigned', {room});
-      };
+        socket.emit("host-assigned", { room });
+      }
+
+      // if room was deleted, recreate empty structure
+      if (!rooms[room]) {
+        rooms[room] = { red: new Set(), blue: new Set() };
+      }
+
       cb(`You've joined to room '${room}'`);
     });
 
-    socket.on('leave-room', (room,cb) => {
+    socket.on("leave-room", (room, cb) => {
       socket.leave(room);
 
-      //Delete team if required
-      if (rooms[room] ){
+      //Remove from team
+      if (rooms[room]) {
         rooms[room].red.delete(socket.id);
         rooms[room].blue.delete(socket.id);
+
+        const redSize = rooms[room].red.size;
+        const blueSize = rooms[room].blue.size;
+
+        if (redSize === 0 && blueSize == 0) {
+          delete rooms[room];
+          delete roomHosts[room];
+          console.log(`Room '${room}' deleted (empty)`);
+        }
       }
       emitTeamState(io, room);
       cb(`You've left room ${room}`);
-    })
-
-
+    });
 
     //join team event
-    socket.on("join-team", ({ room, team }, cb) => {
-      if (!rooms[room]) {
-        rooms[room] = { red: new Set(), blue: new Set() };
+    socket.on("join-team", ({room,team}, cb) => {
+      if (!rooms[room]){
+        room[room] = { red: new Set(), blue: new Set()};
       }
 
       const teamSet = rooms[room][team];
       if (!teamSet) return cb("Invalid team");
       if (teamSet.size >= MAX_TEAM_SIZE) return cb("Team is full");
 
+      //remove from opposite team
+      const otherTeam = team === 'red' ? 'blue' : 'red';
+      rooms[room][otherTeam].delete(socket.id);
+
+      //add to chosen team
       teamSet.add(socket.id);
       socket.join(room);
-      cb(`You joined the ${team} team in room '${room}'`);
+
+      cb(`You joined team ${team} in room ${room}`)
       emitTeamState(io,room);
-    });
+    })
   });
 }
-
-
-
-
 
 function emitTeamState(io, room) {
   const red = Array.from(rooms[room]?.red || []);
